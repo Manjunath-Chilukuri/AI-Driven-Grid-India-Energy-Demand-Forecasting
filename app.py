1d7e6b5f727b1e8ef1c1521ea6d5767e01584ec2
Manjunath-Chilukuri <157456566+Manjunath-Chilukuri@users.noreply.github.com>
2026-07-31 17:51:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 from PIL import Image
 
+import sklearn
+import numpy
+import joblib
+import streamlit as st
+
+st.write("scikit-learn:", sklearn.__version__)
+st.write("numpy:", numpy.__version__)
+st.write("joblib:", joblib.__version__)
+
 # --------------------------------------------------
 # PAGE CONFIGURATION
 # --------------------------------------------------
